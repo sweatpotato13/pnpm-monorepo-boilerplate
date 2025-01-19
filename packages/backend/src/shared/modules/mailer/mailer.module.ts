0c08b235ff60469abe642d12bd90075e108911b1
Cute_Wisp <sweatpotato13@gmail.com>
2025-01-19 11:29:37 +0900

    feat: 🎸 add mailer module in backend package

---
+++ packages/backend/src/shared/modules/mailer/mailer.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MailerModule as NestMailerModule } from "@nestjs-modules/mailer";
+import { MailerConfig } from "@src/config";
+import { MailerConfigService } from "@src/config/modules/mailer/mailer.config.service.ts";
+
+import { MailerService } from "./mailer.service";
+
+@Module({
+    imports: [
+        ConfigModule.forFeature(MailerConfig),
+        NestMailerModule.forRootAsync({
+            imports: [ConfigModule.forFeature(MailerConfig)],
+            useClass: MailerConfigService
+        })
+    ],
+    providers: [
+        {
+            provide: "MailerService",
+            useClass: MailerService
+        }
+    ],
+    exports: ["MailerService"]
+})
+export class MailerModule {}
